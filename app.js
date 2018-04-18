615dd96c036372b1cb0086fa06904782cd52ba1d
ysw0029 <ysw0029@gmail.com>
2018-04-18 16:01:00 +0900

    connect webpurify

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const WebPurify = require('webpurify');
 const wp = new WebPurify({
     api_key: '02bd844833e5a5f1fc15ebb1fbd196f5'
     //, endpoint:   'us'  // Optional, available choices: 'eu', 'ap'. Default: 'us'.
-    //, enterprise: false // Optional, set to true if you are using the enterprise API, allows SSL
+    , enterprise: true // Optional, set to true if you are using the enterprise API, allows SSL
 });
 var app = express();
 
@@ -16,15 +16,22 @@ app.engine('html', require('ejs').renderFile);
 app.get('/', function(req, res){
     res.render('frontend.html',  {'Content-type' : 'text/css'});
 });
+
 app.use(function(req, res){
-    wp.getBlacklist()
-    .then(blacklist => {
-      for (word in blacklist) {
-        console.log(blacklist[word]);
-      }
+    wp.addToBlacklist('my_word')
+    .then(success => {
+      if (success) { console.log('success!'); list();}
     });
 });
 
+function list(){
+    wp.getBlacklist()
+     .then(blacklist => {
+        for (word in blacklist) {
+    console.log(blacklist[word]);
+  }
+});
+}
 var server = app.listen(5000, function(){
     console.log("connecting server using port 5000");
 })
