ecf71cd7bdaa82119a8256d4e4686de3564d3214
ysw0029 <ysw0029@gmail.com>
2018-04-18 14:37:52 +0900

    add css

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const WebPurify = require('webpurify');
+const wp = new WebPurify({
+    api_key: '02bd844833e5a5f1fc15ebb1fbd196f5'
+    //, endpoint:   'us'  // Optional, available choices: 'eu', 'ap'. Default: 'us'.
+    //, enterprise: false // Optional, set to true if you are using the enterprise API, allows SSL
+});
 var app = express();
 
 app.use(express.static('public'));
@@ -11,6 +16,15 @@ app.engine('html', require('ejs').renderFile);
 app.get('/', function(req, res){
     res.render('frontend.html',  {'Content-type' : 'text/css'});
 });
+app.use(function(req, res){
+    wp.getBlacklist()
+    .then(blacklist => {
+      for (word in blacklist) {
+        console.log(blacklist[word]);
+      }
+    });
+});
+
 var server = app.listen(5000, function(){
     console.log("connecting server using port 5000");
-  })
+})
